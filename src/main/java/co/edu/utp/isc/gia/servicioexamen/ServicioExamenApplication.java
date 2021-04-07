a41386f3102225c1fd1dac41268adecf0984b57f
Juan Camilo Chavarro <j.chavarro@utp.edu.co>
2021-04-06 19:22:02 -0500

    Creacion del proyecto Servicio-Examen

---
+++ src/main/java/co/edu/utp/isc/gia/servicioexamen/ServicioExamenApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.servicioexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/isc/gia/servicioexamen/ServicioExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.servicioexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
